f0f3df2db894892b809b492aa186b8ccc72554f8
Roberto Liccardo <roberto_liccardo@outlook.it>
2023-10-10 09:55:52 +0200

    Initial Commit

---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Roberto Liccardo on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
